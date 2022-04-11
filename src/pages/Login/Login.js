778def0ca59efe5fd8c19710d0786c4229137d07
doyu <doyu@DESKTOP-NPAPSDE.localdomain>
2022-04-11 21:00:35 +0900

    JS: check id & pwd validation

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,29 +1,54 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-const Login = (props) => {
-  const [id, setId] = useState("")
-  const [pwd, setPwd] = useState("")
-  return (
+const Login = () => {
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [validation, setValidation] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleIdInput = (e) => {
+    setId(e.target.value);
+  };
+
+  const handlePwdInput = (e) => {
+    setPwd(e.target.value);
+  };
+
+  const checkIdAndPwd = () => {
+    id.includes("@") && pwd.length >= 5
+      ? setValidation(true)
+      : setValidation(false);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+    checkIdAndPwd();
+  };
+
+  return validation && submitted ? (
+    <Navigate to="/main" replace={true} />
+  ) : (
     <div className="container">
       <header className="login-title">Westagram</header>
-      <form action="./main.html" className="login-form">
+      <form onSubmit={onSubmit} action="./main.html" className="login-form">
         <input
+          onKeyUp={handleIdInput}
           className="login-id"
           type="text"
           placeholder="전화번호, 사용자 이름 또는 이메일"
         />
         <input
+          onKeyUp={handlePwdInput}
           className="login-password"
           type="password"
           placeholder="비밀번호"
         />
-        <Link to="/main">
-          <button className="login-button" disabled>
-            로그인
-          </button>
-        </Link>
+        <button className={`login-button ${id && pwd ? "" : "disable"}`}>
+          로그인
+        </button>
       </form>
       <a className="find" href="#">
         비밀번호를 잊으셨나요?
