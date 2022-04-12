5bed6b249ff447e2c1ad65d3f873fb294110e721
doyu <doyu@DESKTOP-NPAPSDE.localdomain>
2022-04-12 14:27:40 +0900

    SCSS: Navbar(css -> scss)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,32 +2,32 @@ import React from "react";
 
 const Navbar = (props) => {
   return (
-    <nav className="nav-bar">
-      <div className="nav-bar__container">
-        <div className="nav-bar__left">
-          <button className="nav-bar__logo">
+    <nav className="navBar">
+      <div className="navBar-container">
+        <div className="navBar-left">
+          <button className="navBar-logo">
             <i className="fab fa-instagram"></i>
           </button>
-          <div className="vertical-bar"></div>
-          <span className="nav-bar__title">Westagram</span>
+          <div className="navBar-verticalBar"></div>
+          <span className="navBar-title">Westagram</span>
         </div>
-        <div className="nav-bar__center">
-          <input type="text" className="search-bar" placeholder="🔍 검색" />
+        <div className="navBar-center">
+          <input type="text" className="navBar-search" placeholder="🔍 검색" />
         </div>
-        <div className="nav-bar__right">
-          <button className="nav-bar__compass">
+        <div className="navBar-right">
+          <button className="navBar-compass">
             <img
               src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
               alt=""
             />
           </button>
-          <button className="nav-bar__heart">
+          <button className="navBar-heart">
             <img
               src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
               alt=""
             />
           </button>
-          <button className="nav-bar__user">
+          <button className="navBar-user">
             <img
               src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
               alt=""
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import FeedList from "../../components/FeedList/FeedList";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Main.scss";
+import "../../components/Navbar/Navbar.scss";
 
 const Main = (props) => {
   return (
